26d1d1d0202ae2101585a33a4326d5c6371129a2
Dmitrij <kramkov@ncpi.gov.by>
2021-04-13 13:31:57 +0300

    feat: added all documents for using and icons

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,76 +1,92 @@
-let currentPath = "../../assets/documents/Алімпіядныя заданні";
+let currentPath = "../../assets/documents";
 
 function showFiles(files){
     $("#files-list").empty();
     files.forEach(file => {
-        type = file.split(".");
-        if(isTypeForDownload(type[type.length - 1])){
+        let fileNameSplit = file.split(".");
+        let extensionFile = fileNameSplit[fileNameSplit.length - 1];
+        let nameIconExtension = getNameIconExtension(extensionFile);
+
+        if(nameIconExtension !== "icon-folder"){
             $("#files-list").append( `
-                <li class="materials-list__item">
+                <li class="materials-list__item icon ${nameIconExtension}">
                     <a class="materials-list__link" href="${currentPath}/${file}" download>${file}</a>
                 </li>
             `);
         }else{
             $("#files-list").append( `
-            <li class="materials-list__item">
-                <p class="materials-list__link link-folder" >${file}</p>
+            <li class="materials-list__item icon ${nameIconExtension}">
+                <p class="materials-list__link link-folder">${file}</p>
             </li>
         `);
         }
     });
 }
-function isTypeForDownload(type){
-    switch(type) {
-        case 'jpg':
-        case 'png':
-        case 'gif':
+function getNameIconExtension(extension){
+    switch(extension) {
+        case 'pptx':
+        case 'ppt':
+            return "icon-ppt";
+        case 'wav':
+        case 'mp3':
+            return "icon-music";
         case 'zip':
         case 'rar':
+            return "icon-rar";
         case 'pdf':
-        case 'php':
+            return "icon-pdf";
+        case 'rtf':
         case 'doc':
         case 'docx':
-        case 'xls':
-        case 'xlsx':
-            return true;
+            return "icon-word";
         default:
-            return false;
+            return "icon-folder";
     }
 }
-$("#go-back").click( function() {
-    let splitUrl = url.split("/");
+$(".go-back-js").click( function() {
+    let splitUrl = currentPath.split("/");
     if(splitUrl.pop() === ""){
         splitUrl.pop();
     }
-    url = splitUrl.join("/") + "/";
-    showMaterialsList();
+    currentPath = splitUrl.join("/");
+    getData();
 });
 $('body').on('click', '.link-folder', function() {
     const folderName = $(this).text();
-    url = `${url}${folderName}/`;
-    showMaterialsList();
+    currentPath = `${currentPath}/${folderName}`;
+    getData();
 });
 getData();
 function getData(){
     $.getJSON( "../../assets/data/roots.json", function( data ) {
         let array = new Array();
-        
+
         let currentPathSplit = currentPath.split("/");
         let currentFolder = currentPathSplit[currentPathSplit.length - 1];
 
+        let targetPathSplit = currentPath.split("assets/");
+        let targetPath = targetPathSplit[targetPathSplit.length - 1];
+
         data.forEach(element => {
-            
-            let splitElementPath = `${element.Path}/${element.Name}`.split("/");
-            let folderPozition = splitElementPath.indexOf(currentFolder);
-            
-            if(folderPozition >= 0){
-                let nextElement = splitElementPath[folderPozition + 1];
-                
-                if(!array.includes(nextElement)){
-                    array.push(nextElement);
+            if(element.Path.includes(targetPath)){
+                let splitElementPath = `${element.Path}/${element.Name}`.split("/");
+                let folderPozition = splitElementPath.indexOf(currentFolder);
+
+                if(folderPozition >= 0){
+                    let nextElement = splitElementPath[folderPozition + 1];
+
+                    if(!array.includes(nextElement)){
+                        array.push(nextElement);
+                    }
                 }
             }
         });
         showFiles(array);
       });
+
+      if(currentPath === "../../assets/documents"){
+        $(".go-back-js").hide();
+      }else{
+        $(".go-back-js").show();
+      }
 }
