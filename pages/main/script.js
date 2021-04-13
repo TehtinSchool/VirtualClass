a97010a26b65f2e1dcb57588e3958b820a353dd5
Dmitrij <kramkov@ncpi.gov.by>
2021-04-13 16:33:58 +0300

    feat: add link for view

--- pages/main/script.js
+++ pages/main/script.js
@@ -8,23 +8,38 @@ function showFiles(files){
         let extensionFile = fileNameSplit[fileNameSplit.length - 1];
         let nameIconExtension = getNameIconExtension(extensionFile);
 
-        if(nameIconExtension !== "icon-folder"){
+        if(nameIconExtension === "icon-folder"){
+            $("#files-list").append( `
+                <li class="materials-list__item icon ${nameIconExtension}">
+                    <p class="materials-list__link link-folder">${file}</p>
+                </li>
+            `);
+
+        }
+        else if(nameIconExtension === "icon-link"){
+            let splitName = file.split(".link");
+            file = splitName[0];
+            getLinkByName(file).then( link => {
+                $("#files-list").append( `
+                    <li class="materials-list__item icon ${nameIconExtension}">
+                        <a class="materials-list__link" href="${link}">${file}</a>
+                    </li>
+                `);
+            });
+        }
+        else{
             $("#files-list").append( `
                 <li class="materials-list__item icon ${nameIconExtension}">
                     <a class="materials-list__link" href="${currentPath}/${file}" download>${file}</a>
                 </li>
             `);
-        }else{
-            $("#files-list").append( `
-            <li class="materials-list__item icon ${nameIconExtension}">
-                <p class="materials-list__link link-folder">${file}</p>
-            </li>
-        `);
         }
     });
 }
 function getNameIconExtension(extension){
     switch(extension) {
+        case 'link':
+            return "icon-link";
         case 'pptx':
         case 'ppt':
             return "icon-ppt";
@@ -90,4 +105,19 @@ function getData(){
     else{
       $(".go-back-js").show();
     }
+}
+
+
+async function getLinkByName(name){
+    let res = null;
+
+    const req = await fetch("../../assets/data/links.json");
+    const data = await req.json();
+
+    data.forEach(element => {
+        if(element.Name === name){
+            res = element.Link;
+        }
+    });
+    return res;
 }
